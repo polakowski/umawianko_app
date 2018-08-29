e9fdc1b398766c075e85362cd259895ef12d4b02
marekpolakowski <marek.polakowski@gmail.com>
2018-08-29 18:16:06 +0200

    UM-2 allow to update event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -25,6 +25,23 @@ class EventsController < AuthenticatedController
     @event_user = @event.event_users.find_by(user: current_user)
   end
 
+  def edit
+    @resource = Event.find(params[:id])
+    @event = EventForm::CreateEvent.new(@resource)
+  end
+
+  def update
+    @resource = Event.new
+    @event = EventForm::CreateEvent.new(@resource, event_params).as(current_user)
+
+    if @event.save
+      flash[:success] = 'Successfully updated event!'
+      redirect_to @event
+    else
+      render :edit
+    end
+  end
+
   private
 
   def event_params
