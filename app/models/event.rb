2445ca74fc7ddd96c78eda33a08857d037c644d6
Marek Polakowski <9076315+polakowski@users.noreply.github.com>
2019-07-03 14:09:53 +0200

    UM-22 feat: delete event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,10 @@
 class Event < ApplicationRecord
+  acts_as_paranoid
+
   UNTAGGED_COLOR = '#bfbfbf'.freeze
 
   belongs_to :creator, class_name: 'User', inverse_of: :created_events
-  has_many :event_users, dependent: :restrict_with_exception
+  has_many :event_users, dependent: :destroy
   has_many :users_joined, through: :event_users, source: :user
   belongs_to :event_type, inverse_of: :events, optional: true
 
@@ -16,6 +18,8 @@ class Event < ApplicationRecord
            :future?,
            to: :datetime,
            allow_nil: true
+
+  delegate :slack_webhook, to: :event_type, allow_nil: true
 
   def participants_count
     users_joined.count + friends_count
@@ -31,5 +35,9 @@ class Event < ApplicationRecord
 
   def datetime
     super&.in_time_zone
+  end
+
+  def slack_webhook
+    event_type&.slack_webhook
   end
 end
